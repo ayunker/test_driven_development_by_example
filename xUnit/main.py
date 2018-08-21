3454947626ebbe5134993e45035f4a52d26f806f
Tony Yunker <tony.yunker@gmail.com>
2018-08-20 19:55:23 -0500

    chapter 22

--- xUnit/main.py
+++ xUnit/main.py
@@ -12,8 +12,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
 
@@ -39,15 +42,16 @@ class WasRun(TestCase):
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
-    def summary(self):
-        return "%d run, 0 failed" % self.runCount
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -58,7 +62,13 @@ class TestCaseTest(TestCase):
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
-        assert("1 run, 1 failed", result.summary)
+        assert "1 run, 1 failed", result.summary
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testFailedResult").run()
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert "1 run, 1 failed", result.summary
+
+# TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testFailedResultFormatting").run()
